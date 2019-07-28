b3165a70bbd37d62edad06dd8a6c0d6fa8e5a469
dulei <duleiland@126.com>
2019-07-28 22:46:34 +0800

    添加Javassist动态生成类，动态添加注解undergoing

--- JavassitDemo/src/main/java/com/landmine/code/CreateNewClassDemo.java
+++ JavassitDemo/src/main/java/com/landmine/code/CreateNewClassDemo.java
@@ -1,6 +1,16 @@
 package com.landmine.code;
 
 import javassist.*;
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.annotation.Annotation;
+import lombok.ToString;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * author: lanrish
@@ -11,24 +21,48 @@ import javassist.*;
  * </pre>
  */
 public class CreateNewClassDemo {
-    public static void main(String[] args) throws CannotCompileException, NotFoundException {
+    public static void main(String[] args) throws CannotCompileException, NotFoundException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, IOException {
         ClassPool pool = ClassPool.getDefault();
         CtClass ctClass = pool.makeClass("com.landmine.code.Student");
+        ClassFile ccFile = ctClass.getClassFile();
+        ConstPool constPool = ccFile.getConstPool();
+
+        AnnotationsAttribute classAttr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
+        Annotation annotation = new Annotation("lombok.ToString", constPool);
+        classAttr.addAnnotation(annotation);
+        ccFile.addAttribute(classAttr);
+
         //创建属性
-        CtField field1 = CtField.make("private String name", ctClass);
-        CtField field2 = CtField.make("private int age", ctClass);
+        CtField field1 = CtField.make("private String name;", ctClass);
+        CtField field2 = CtField.make("private int age;", ctClass);
         ctClass.addField(field1);
         ctClass.addField(field2);
 
         //创建方法
-        String src;
         CtMethod setName = CtNewMethod.make("public void setName(String name){this.name = name;}", ctClass);
-        CtMethod getName = CtNewMethod.make("public String getName(){return name;}", ctClass);
+        CtMethod getName = CtNewMethod.make("public String getName(){return this.name;}", ctClass);
         ctClass.addMethod(setName);
         ctClass.addMethod(getName);
 
+
         //添加构造器
-        CtConstructor constructor = new CtConstructor(new CtClass[]{CtClass.intType, pool.get("java.lang.String")}, ctClass);
-        constructor.setBody("this.num= num; this.name");
+        CtConstructor  constructor =  new CtNewConstructor().make("" +
+                "public Student(int age, String name){" +
+                "   this.age = age;" +
+                "   this.name = name;" +
+                "}",ctClass);
+
+        ctClass.addConstructor(constructor);
+        ctClass.writeFile("E:\\IdeaProjects\\StudyDemo\\JavassitDemo\\target\\classes\\");
+        Class<?> clazz = ctClass.toClass();
+        Constructor cons = clazz.getDeclaredConstructor(int.class,String.class);
+        Object o = cons.newInstance(24, "lanrish");
+        System.out.println(clazz.getCanonicalName());
+        Method getMethod = clazz.getMethod("getName");
+        System.out.println(getMethod.invoke(o));
+        Method toStringMethod = clazz.getMethod("toString");
+        System.out.println(toStringMethod.invoke(o));
+
+
     }
 }
